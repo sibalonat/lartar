60e8b029fcd27f9aab6c83b1ab23039e9c79b779
Marin Nikolli <sibalonat@gmail.com>
2025-03-05 00:50:01 +0100

    Add desktop serving commands and Vite configuration for Tauri application

--- app/Console/Commands/ServeDesktop.php
+++ app/Console/Commands/ServeDesktop.php
@@ -3,6 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use function Laravel\Prompts\note;
+use function Laravel\Prompts\intro;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Process;
 
 class ServeDesktop extends Command
 {
@@ -25,6 +29,28 @@ class ServeDesktop extends Command
      */
     public function handle()
     {
-        //
+        intro( 'Running Desktop Environment' );
+
+        $this->initViteServer();
+        $this->initTauriServer();
+    }
+
+    private function initTauriServer() : void
+    {
+        note( 'Starting Desktop App' );
+
+        if( ! File::exists( base_path( 'src-tauri/target' ) ) )
+        {
+            Process::path( 'src-tauri' )->forever()->tty()->run( "cargo build" );
+        }
+
+        Process::forever()->tty()->run( "npm run dev:tauri:desktop -- --port=50003" );
+    }
+
+    private function initViteServer() : void
+    {
+        note( "Starting Vite Development Server" );
+
+        Process::start( "npm run dev:vite:desktop" );
     }
 }
---
+++ vite.desktop.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+
+export default defineConfig( {
+    plugins : [
+        laravel( {
+            input : 'resources/js/app.js',
+            refresh : true,
+        } ),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    clearScreen : false,
+    server : {
+        port : 50002
+    }
+} );
